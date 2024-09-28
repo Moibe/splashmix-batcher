efb37ac98947c477c8439df4879c3a1ccdd2373f
Moibe <moi_estrello@hotmail.com>
2024-09-28 00:45:33 -0600

    Test concurrect futures

--- 4.-FullProcess.py
+++ 4.-FullProcess.py
@@ -1,6 +1,6 @@
-import intertools, configuracion.configuracion as configuracion, time
+import intertools 
 import configuracion.globales as globales
-import pandas as pd
+import configuracion.configuracion as configuracion
 
 #CICLO INTERMEDIO = STABLE DIFFUSION
 
@@ -12,11 +12,7 @@ filename = configuracion.sesion + '.xlsx'
 
 #Hacer el Stable Diffusion.
 #Future: Las características importantes deberían pasarse desde aquí... (que objeto, etc.)
-#inicial indica el archivo desde donde debe empezar.
 
-#FULL ES LA QUE HACE EL STABLE DIFF
-#Future: Corregir para que empiece exacto.
-#Debes de poner en inicial LA ULTIMA QUE SI SE HIZO... si quieres inicial va así: inicial="IsaRomo-t1.jpg"
 #FUTURE: Que samples venga de configuración para evitar discrepancias entre preProcess y fullProcess.
 #IMPORTANTE: Una alternativa a empezar desde un archivo en particular, es correr mi función missing...
 #... que generará una columna de imagenes de todas aquellas que no han sido procesadas (dejando fuera a las complete
