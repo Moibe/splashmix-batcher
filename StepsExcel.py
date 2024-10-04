bbe62a74f4c57df3eaacb1eee04ec6639fdee5a3
Moibe <moi_estrello@hotmail.com>
2024-10-04 17:44:41 -0600

    Sube Sources via Excels Steps

--- StepsExcel.py
+++ StepsExcel.py
@@ -12,7 +12,12 @@ filename = sesion + '.xlsx'
 # respuesta = input("Presiona cualquier tecla para continuar: ")
 # pretools.creaExcel(filename)
 
-print("A continuación descargaremos las imagenes del lote...")
+# print("A continuación descargaremos las imagenes del lote...")
+# respuesta = input("Presiona cualquier tecla para continuar: ")
+# pretools.descargaImagenes(sesion)
+
+print("A continuación subiremos las imagenes SOURCES a mi servidor...")
 respuesta = input("Presiona cualquier tecla para continuar: ")
-pretools.descargaImagenes(sesion)
+#Future: Debería tener dos procesos separados: SUBIR/REGISTRAR.
+pretools.subeSources()    
     
