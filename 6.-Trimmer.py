9205e2b363cbaa5182658bc9da809ed6e543237a
Moibe <moi_estrello@hotmail.com>
2024-10-08 11:40:41 -0600

    Trimmer Adaptado

---
+++ 6.-Trimmer.py
@@ -0,0 +1,15 @@
+import tools
+import pandas as pd
+import configuracion.globales as globales
+import configuracion.configuracion as configuracion 
+
+excel = globales.excel_results_path + configuracion.sesion + '.xlsx'
+#Primero extraemos el dataframe:
+dataframe = pd.read_excel(excel)  
+
+# Eliminar las columnas
+columnas_a_eliminar = ['Source Path', 'Name', 'File', 'File Path', 'style', 'subject', 'Shot']
+dataframe.drop(columnas_a_eliminar, axis=1, inplace=True)
+
+# Guardar el DataFrame en un nuevo archivo Excel
+tools.df2Excel(dataframe, configuracion.sesion + 'deliver.xlsx')
